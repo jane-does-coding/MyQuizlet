8937b04577681bcb08d754615dbcf9d4dafbe829
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-07-30 21:05:10 -0600

    grid and cards

--- app/components/GridCard.tsx
+++ app/components/GridCard.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 
-const GridCard = () => {
+const GridCard = ({ card }: any) => {
 	return (
-		<div className="w-full bg-zinc-800 rounded-md py-4 px-4">
-			<h2 className="text-neutral-200">Hello</h2>
+		<div className="w-full bg-zinc-800 rounded-md py-4 pb-6 px-4">
+			<h2 className="text-neutral-100 mb-3 text-[1.25rem]">{card.title}</h2>
 			<span className="bg-yellow-300 text-neutral-800 px-2 py-[1px] rounded-full">
-				34 questions
+				{card.cards} questions
 			</span>
+			<div className="flex gap-2 items-center justify-start mt-6">
+				<img
+					src="/avatar.png"
+					className="w-[30px] h-[30px] object-cover rounded-full"
+					alt=""
+				/>
+				<h2 className="text-neutral-300">{card.author}</h2>
+			</div>
 		</div>
 	);
 };
--- app/components/Pages/Quizzes.tsx
+++ app/components/Pages/Quizzes.tsx
@@ -2,12 +2,29 @@ import React from "react";
 import GridCard from "../GridCard";
 
 const Quizzes = () => {
+	const cards = [
+		{
+			title: "Medical Prefixes",
+			cards: 4,
+			author: "Lorem Ipsum",
+		},
+		{
+			title: "Body Systems",
+			cards: 12,
+			author: "Lorem Ipsum",
+		},
+		{
+			title: "Lego",
+			cards: 32,
+			author: "Lorem Ipsum",
+		},
+	];
+
 	return (
 		<div className="grid grid-cols-3 w-[90%] mx-auto gap-4">
-			<GridCard />
-			<GridCard />
-			<GridCard />
-			<GridCard />
+			{cards.map((card) => (
+				<GridCard card={card} />
+			))}
 		</div>
 	);
 };
